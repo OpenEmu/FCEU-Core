98acf529e88462c0861e976b9a1e5a578c517811
clobber <brymaster@gmail.com>
2015-02-27 03:58:36 -0600

    Update to FCEUX 2.2.2

--- FCEUGameCore.mm
+++ FCEUGameCore.mm
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2009, OpenEmu Team
+ Copyright (c) 2015, OpenEmu Team
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
@@ -29,180 +29,77 @@
 #import "OENESSystemResponderClient.h"
 #import <OpenGL/gl.h>
 
-#include "libsnes.h"
+#include "src/fceu.h"
+#include "src/driver.h"
+#include "src/palette.h"
+#include "src/state.h"
+
+extern uint8 *XBuf;
+static uint32_t palette[256];
 
 @interface FCEUGameCore () <OENESSystemResponderClient>
 {
-    uint16_t *videoBuffer;
-    int videoWidth, videoHeight;
-    int16_t pad[2][16];
-    NSString *romName;
-    double sampleRate;
+    uint32_t *videoBuffer;
+    uint8_t *pXBuf;
+    int32_t *soundBuffer;
+    int32_t soundSize;
+    uint32_t pad[2][OENESButtonCount];
 }
 
 @end
-
-NSUInteger FCEUEmulatorValues[] = { SNES_DEVICE_ID_JOYPAD_UP, SNES_DEVICE_ID_JOYPAD_DOWN, SNES_DEVICE_ID_JOYPAD_LEFT, SNES_DEVICE_ID_JOYPAD_RIGHT, SNES_DEVICE_ID_JOYPAD_A, SNES_DEVICE_ID_JOYPAD_B, SNES_DEVICE_ID_JOYPAD_START, SNES_DEVICE_ID_JOYPAD_SELECT };
-NSString *FCEUEmulatorKeys[] = { @"Joypad@ Up", @"Joypad@ Down", @"Joypad@ Left", @"Joypad@ Right", @"Joypad@ A", @"Joypad@ B", @"Joypad@ Start", @"Joypad@ Select"};
 
 @implementation FCEUGameCore
 
 static __weak FCEUGameCore *_current;
 
-static void audio_callback(uint16_t left, uint16_t right)
-{
-    GET_CURRENT_AND_RETURN();
-
-    [[current ringBufferAtIndex:0] write:&left maxLength:2];
-    [[current ringBufferAtIndex:0] write:&right maxLength:2];
-}
-
-static void video_callback(const uint16_t *data, unsigned width, unsigned height)
-{
-    GET_CURRENT_AND_RETURN();
-
-    // Normally our pitch is 2048 bytes.
-    int stride = 1024;
-
-    // If we have an interlaced mode, pitch is 1024 bytes.
-    if(height == 256 || height == 478)
-        stride = 256;
-
-    current->videoWidth  = width;
-    current->videoHeight = height;
-
-    dispatch_queue_t the_queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-
-    dispatch_apply(height, the_queue, ^(size_t y) {
-        const uint16_t *src = data + y * stride;
-        uint16_t *dst = current->videoBuffer + y * 256;
-
-        memcpy(dst, src, sizeof(uint16_t)*width);
-    });
-}
-
-static void input_poll_callback(void)
-{
-	//NSLog(@"poll callback");
-}
-
-static int16_t input_state_callback(bool port, unsigned device, unsigned index, unsigned devid)
-{
-    GET_CURRENT_AND_RETURN(0);
-
-    //NSLog(@"polled input: port: %d device: %d id: %d", port, device, devid);
-
-	if(port == SNES_PORT_1 & device == SNES_DEVICE_JOYPAD)
-        return current->pad[0][devid];
-    else if(port == SNES_PORT_2 & device == SNES_DEVICE_JOYPAD)
-        return current->pad[1][devid];
-
-    return 0;
-}
-
-static bool environment_callback(unsigned cmd, void *data)
-{
-    GET_CURRENT_AND_RETURN(true);
-
-    switch(cmd)
-    {
-        case SNES_ENVIRONMENT_SET_TIMING:
-        {
-            snes_system_timing *t = (snes_system_timing *)data;
-            current->frameInterval = t->fps;
-            current->sampleRate    = t->sample_rate;
-            return YES;
-        }
-        case SNES_ENVIRONMENT_GET_FULLPATH:
-        {
-            *(const char **)data = [current->romName UTF8String];
-            NSLog(@"Environ FULLPATH: \"%@\"\n", current->romName);
-            break;
-        }
-        default:
-            NSLog(@"Environ UNSUPPORTED (#%u)!\n", cmd);
-            return YES;
-    }
-
-    return true;
-}
-
-static void loadSaveFile(const char *path, int type)
-{
-    FILE *file;
-
-    file = fopen(path, "rb");
-    if(file == NULL) return;
-
-    size_t size = snes_get_memory_size(type);
-    uint8_t *data = snes_get_memory_data(type);
-
-    if(size == 0 || data == NULL)
-    {
-        fclose(file);
-        return;
-    }
-
-    int rc = fread(data, sizeof(uint8_t), size, file);
-    if(rc != size)
-        NSLog(@"Couldn't load save file: %s.", path);
-    else
-        NSLog(@"Loaded save file: %s", path);
-
-    fclose(file);
-}
-
-static void writeSaveFile(const char *path, int type)
-{
-    size_t size = snes_get_memory_size(type);
-    uint8_t *data = snes_get_memory_data(type);
-
-    if(data != NULL && size > 0)
-    {
-        FILE *file = fopen(path, "wb");
-        if(file != NULL)
-        {
-            NSLog(@"Saving state %s. Size: %d bytes.", path, (int)size);
-            if(fwrite(data, sizeof(uint8_t), size, file) != size)
-                NSLog(@"Did not save state properly.");
-            fclose(file);
-        }
-    }
-}
-
-- (oneway void)didPushNESButton:(OENESButton)button forPlayer:(NSUInteger)player;
-{
-    pad[player - 1][FCEUEmulatorValues[button]] = 0xFFFF;
-    //pad[player - 1][FCEUEmulatorValues[button]] = 1;
-}
-
-- (oneway void)didReleaseNESButton:(OENESButton)button forPlayer:(NSUInteger)player;
-{
-    pad[player - 1][FCEUEmulatorValues[button]] = 0;
-}
-
-- (void)didPushFDSChangeSideButton;
-{
-
-}
-- (void)didReleaseFDSChangeSideButton;
-{
-
-}
-
 - (id)init
 {
     if((self = [super init]))
     {
-        videoBuffer = (uint16_t *)malloc(256 * 240 * 2);
+        videoBuffer = (uint32_t *)malloc(256 * 240 * 4);
     }
-	
+
 	_current = self;
 
 	return self;
 }
 
-#pragma mark Exectuion
+- (void)dealloc
+{
+    free(videoBuffer);
+}
+
+# pragma mark - Execution
+
+- (BOOL)loadFileAtPath:(NSString *)path error:(NSError **)error
+{
+    memset(pad, 0, sizeof(uint32_t) * OENESButtonCount);
+
+    //newppu = 0 default off, set 1 to enable
+
+    FCEUI_Initialize();
+
+    //FCEUI_SetBaseDirectory([[self biosDirectoryPath] UTF8String]); unused for now
+    FCEUI_SetDirOverride(FCEUIOD_NV, strdup([[self batterySavesDirectoryPath] UTF8String]));
+
+    FCEUI_SetSoundVolume(256);
+    FCEUI_Sound(48000);
+
+    FCEUGI *FCEUGameInfo;
+    FCEUGameInfo = FCEUI_LoadGame([path UTF8String], 1, false);
+
+    if(!FCEUGameInfo)
+        return NO;
+
+    //NSLog(@"FPS: %d", FCEUI_GetDesiredFPS() >> 24); // Hz
+
+    FCEUI_SetInput(0, SI_GAMEPAD, &pad[0], 0);
+    FCEUI_SetInput(1, SI_GAMEPAD, &pad[1], 0);
+
+    FCEU_ResetPalette();
+
+    return YES;
+}
 
 - (void)executeFrame
 {
@@ -211,67 +108,42 @@ static void writeSaveFile(const char *path, int type)
 
 - (void)executeFrameSkippingFrame:(BOOL)skip
 {
-    snes_run();
+    pXBuf = 0;
+    soundSize = 0;
+
+    FCEUI_Emulate(&pXBuf, &soundBuffer, &soundSize, 0);
+
+    pXBuf = XBuf;
+    for (unsigned y = 0; y < 240; y++)
+        for (unsigned x = 0; x < 256; x++, pXBuf++)
+            videoBuffer[y * 256 + x] = palette[*pXBuf];
+
+    for (int i = 0; i < soundSize; i++)
+        soundBuffer[i] = (soundBuffer[i] << 16) | (soundBuffer[i] & 0xffff);
+
+    [[self ringBufferAtIndex:0] write:soundBuffer maxLength:soundSize << 2];
 }
 
-- (BOOL)loadFileAtPath:(NSString *)path error:(NSError **)error
+- (void)resetEmulation
 {
-	memset(pad, 0, sizeof(int16_t) * 16);
-
-    uint8_t *data;
-    unsigned size;
-    romName = [path copy];
-
-    //load cart, read bytes, get length
-    NSData *dataObj = [NSData dataWithContentsOfFile:[romName stringByStandardizingPath]];
-    if(dataObj == nil) return NO;
-
-    size = [dataObj length];
-    data = (uint8_t *)[dataObj bytes];
-
-    //remove copier header, if it exists
-    //ssif((size & 0x7fff) == 512) memmove(data, data + 512, size -= 512);
-
-    //memory.copy(data, size);
-    snes_set_environment(environment_callback);
-	snes_init();
-	
-    snes_set_audio_sample(audio_callback);
-    snes_set_video_refresh(video_callback);
-    snes_set_input_poll(input_poll_callback);
-    snes_set_input_state(input_state_callback);
-	
-    if(snes_load_cartridge_normal(NULL, data, size))
-    {
-        NSString *path = romName;
-        NSString *extensionlessFilename = [[path lastPathComponent] stringByDeletingPathExtension];
-
-        NSString *batterySavesDirectory = [self batterySavesDirectoryPath];
-
-        //if((batterySavesDirectory != nil) && ![batterySavesDirectory isEqualToString:@""])
-        if([batterySavesDirectory length] != 0)
-        {
-            [[NSFileManager defaultManager] createDirectoryAtPath:batterySavesDirectory withIntermediateDirectories:YES attributes:nil error:NULL];
-
-            NSString *filePath = [batterySavesDirectory stringByAppendingPathComponent:[extensionlessFilename stringByAppendingPathExtension:@"sav"]];
-
-            loadSaveFile([filePath UTF8String], SNES_MEMORY_CARTRIDGE_RAM);
-        }
-
-        snes_set_controller_port_device(SNES_PORT_1, SNES_DEVICE_JOYPAD);
-        snes_set_controller_port_device(SNES_PORT_2, SNES_DEVICE_JOYPAD);
-
-        snes_get_region();
-
-        snes_run();
-
-        return YES;
-    }
-
-    return NO;
+    ResetNES();
 }
 
-#pragma mark Video
+- (void)stopEmulation
+{
+    FCEUI_CloseGame();
+    FCEUI_Kill();
+
+    [super stopEmulation];
+}
+
+- (NSTimeInterval)frameInterval
+{
+    return FCEUI_GetDesiredFPS() / 16777216.0;
+}
+
+# pragma mark - Video
+
 - (const void *)videoBuffer
 {
     return videoBuffer;
@@ -279,46 +151,12 @@ static void writeSaveFile(const char *path, int type)
 
 - (OEIntRect)screenRect
 {
-    return OEIntRectMake(0, 0, videoWidth, videoHeight);
+    return OEIntRectMake(0, 0, 256, 240);
 }
 
 - (OEIntSize)bufferSize
 {
     return OEIntSizeMake(256, 240);
-}
-
-- (void)resetEmulation
-{
-    snes_reset();
-}
-
-- (void)stopEmulation
-{
-    NSString *path = romName;
-    NSString *extensionlessFilename = [[path lastPathComponent] stringByDeletingPathExtension];
-
-    NSString *batterySavesDirectory = [self batterySavesDirectoryPath];
-
-    if([batterySavesDirectory length] != 0)
-    {
-        [[NSFileManager defaultManager] createDirectoryAtPath:batterySavesDirectory withIntermediateDirectories:YES attributes:nil error:NULL];
-
-        NSLog(@"Trying to save SRAM");
-
-        NSString *filePath = [batterySavesDirectory stringByAppendingPathComponent:[extensionlessFilename stringByAppendingPathExtension:@"sav"]];
-
-        writeSaveFile([filePath UTF8String], SNES_MEMORY_CARTRIDGE_RAM);
-    }
-
-    NSLog(@"snes term");
-    snes_unload_cartridge();
-    snes_term();
-    [super stopEmulation];
-}
-
-- (void)dealloc
-{
-    free(videoBuffer);
 }
 
 - (GLenum)pixelFormat
@@ -328,22 +166,19 @@ static void writeSaveFile(const char *path, int type)
 
 - (GLenum)pixelType
 {
-    return GL_UNSIGNED_SHORT_1_5_5_5_REV;
+    return GL_UNSIGNED_INT_8_8_8_8_REV;
 }
 
 - (GLenum)internalPixelFormat
 {
-    return GL_RGB5;
+    return GL_RGB8;
 }
+
+# pragma mark - Audio
 
 - (double)audioSampleRate
 {
-    return sampleRate ? sampleRate : 48000;
-}
-
-- (NSTimeInterval)frameInterval
-{
-    return frameInterval ? frameInterval : 60.099;
+    return FSettings.SndRate;
 }
 
 - (NSUInteger)channelCount
@@ -351,60 +186,93 @@ static void writeSaveFile(const char *path, int type)
     return 2;
 }
 
-- (void)saveStateToFileAtPath:(NSString *)fileName completionHandler:(void (^)(BOOL, NSError *))block
+# pragma mark - Save States
+
+- (BOOL)saveStateToFileAtPath:(NSString *)fileName
 {
-    int serial_size = snes_serialize_size();
-    NSMutableData *stateData = [NSMutableData dataWithLength:serial_size];
-
-    if(!snes_serialize((uint8_t *)[stateData mutableBytes], serial_size))
-    {
-        NSError *error = [NSError errorWithDomain:OEGameCoreErrorDomain code:OEGameCoreCouldNotSaveStateError userInfo:@{
-            NSLocalizedDescriptionKey : @"Save state data could not be written",
-            NSLocalizedRecoverySuggestionErrorKey : @"The emulator could not write the state data."
-        }];
-        block(NO, error);
-        return;
-    }
-
-    __autoreleasing NSError *error = nil;
-    BOOL success = [stateData writeToFile:fileName options:NSDataWritingAtomic error:&error];
-
-    block(success, success ? nil : error);
+    FCEUSS_Save([fileName UTF8String], false);
+    return YES;
 }
 
 - (void)loadStateFromFileAtPath:(NSString *)fileName completionHandler:(void (^)(BOOL, NSError *))block
 {
-    __autoreleasing NSError *error = nil;
-    NSData *data = [NSData dataWithContentsOfFile:fileName options:NSDataReadingMappedIfSafe | NSDataReadingUncached error:&error];
+    int success = FCEUSS_Load([fileName UTF8String], false);
+    if(block) block(success==1, nil);
+}
 
-    if(data == nil)
-    {
-        block(NO, error);
-        return;
-    }
+# pragma mark - Input
 
-    int serial_size = snes_serialize_size();
-    if(serial_size != [data length])
-    {
-        NSError *error = [NSError errorWithDomain:OEGameCoreErrorDomain code:OEGameCoreStateHasWrongSizeError userInfo:@{
-            NSLocalizedDescriptionKey : @"Save state has wrong file size.",
-            NSLocalizedRecoverySuggestionErrorKey : [NSString stringWithFormat:@"The size of the file %@ does not have the right size, %d expected, got: %ld.", fileName, serial_size, [data length]],
-        }];
-        block(NO, error);
-        return;
-    }
+const int NESMap[] = {JOY_UP, JOY_DOWN, JOY_LEFT, JOY_RIGHT, JOY_A, JOY_B, JOY_START, JOY_SELECT};
+- (oneway void)didPushNESButton:(OENESButton)button forPlayer:(NSUInteger)player;
+{
+    int playerShift = player != 1 ? 8 : 0;
 
-    if(!snes_unserialize((uint8_t *)[data bytes], serial_size))
-    {
-        NSError *error = [NSError errorWithDomain:OEGameCoreErrorDomain code:OEGameCoreCouldNotLoadStateError userInfo:@{
-            NSLocalizedDescriptionKey : @"The save state data could not be read",
-            NSLocalizedRecoverySuggestionErrorKey : [NSString stringWithFormat:@"Could not read the file state in %@.", fileName]
-        }];
-        block(NO, error);
-        return;
-    }
-    
-    block(YES, nil);
+    pad[player - 1][0] |= NESMap[button] << playerShift;
+}
+
+- (oneway void)didReleaseNESButton:(OENESButton)button forPlayer:(NSUInteger)player;
+{
+    int playerShift = player != 1 ? 8 : 0;
+
+    pad[player - 1][0] &= ~NESMap[button] << playerShift;
+}
+
+// FCEUX internal functions and stubs
+void FCEUD_SetPalette(unsigned char index, unsigned char r, unsigned char g, unsigned char b)
+{
+    palette[index] = ( r << 16 ) | ( g << 8 ) | b;
+}
+
+void FCEUD_GetPalette(unsigned char i, unsigned char *r, unsigned char *g, unsigned char *b) {}
+uint64 FCEUD_GetTime(void) {return 0;}
+uint64 FCEUD_GetTimeFreq(void) {return 0;}
+const char *GetKeyboard(void) {return "";}
+bool turbo = false;
+int closeFinishedMovie = 0;
+int FCEUD_ShowStatusIcon(void) {return 0;}
+int FCEUD_SendData(void *data, uint32 len) {return 1;}
+int FCEUD_RecvData(void *data, uint32 len) {return 1;}
+FILE *FCEUD_UTF8fopen(const char *fn, const char *mode)
+{
+    return fopen(fn, mode);
+}
+EMUFILE_FILE *FCEUD_UTF8_fstream(const char *fn, const char *m)
+{
+    std::ios_base::openmode mode = std::ios_base::binary;
+    if(!strcmp(m,"r") || !strcmp(m,"rb"))
+        mode |= std::ios_base::in;
+    else if(!strcmp(m,"w") || !strcmp(m,"wb"))
+        mode |= std::ios_base::out | std::ios_base::trunc;
+    else if(!strcmp(m,"a") || !strcmp(m,"ab"))
+        mode |= std::ios_base::out | std::ios_base::app;
+    else if(!strcmp(m,"r+") || !strcmp(m,"r+b"))
+        mode |= std::ios_base::in | std::ios_base::out;
+    else if(!strcmp(m,"w+") || !strcmp(m,"w+b"))
+        mode |= std::ios_base::in | std::ios_base::out | std::ios_base::trunc;
+    else if(!strcmp(m,"a+") || !strcmp(m,"a+b"))
+        mode |= std::ios_base::in | std::ios_base::out | std::ios_base::app;
+    return new EMUFILE_FILE(fn, m);
+    //return new std::fstream(fn,mode);
+}
+void FCEUD_NetplayText(uint8 *text) {};
+void FCEUD_NetworkClose(void) {}
+void FCEUD_VideoChanged (void) {}
+bool FCEUD_ShouldDrawInputAids() {return false;}
+bool FCEUD_PauseAfterPlayback() {return false;}
+void FCEUI_AviVideoUpdate(const unsigned char* buffer) {}
+bool FCEUI_AviEnableHUDrecording() {return false;}
+bool FCEUI_AviIsRecording(void) {return false;}
+bool FCEUI_AviDisableMovieMessages() {return true;}
+FCEUFILE *FCEUD_OpenArchiveIndex(ArchiveScanRecord &asr, std::string &fname, int innerIndex) {return 0;}
+FCEUFILE *FCEUD_OpenArchive(ArchiveScanRecord &asr, std::string &fname, std::string *innerFilename) {return 0;}
+ArchiveScanRecord FCEUD_ScanArchive(std::string fname) { return ArchiveScanRecord(); }
+void FCEUD_PrintError(const char *s)
+{
+    NSLog(@"FCEUX error: %s", s);
+}
+void FCEUD_Message(const char *s)
+{
+    NSLog(@"FCEUX message: %s", s);
 }
 
 @end
